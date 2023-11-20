8f007ae75bd91bdb6dd4a15cb39dcf68d50f6e42
Fran Lara <fran@digidip.net>
2023-11-20 19:04:04 +0100

    Added routes and config

--- app/Http/Controllers/API/Root.php
+++ app/Http/Controllers/API/Root.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\API as APIController;
 use Dingo\Api\Http\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class Root extends APIController
 {
     public function index(): Response
     {
         return $this->response->array(['foo' => 'bar']);
+    }
+
+    public function options(): Response
+    {
+        return (new Response([], Response::HTTP_OK))->header('Allow',
+            implode(', ', [Request::METHOD_GET, Request::METHOD_OPTIONS, Request::METHOD_POST]));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,8 @@
 |--------------------------------------------------------------------------
 */
 
-$api->options('index','App\Http\Controllers\API\Root@index');
+$api->options('{catchall}','App\Http\Controllers\API\Root@options');
+$api->get('index','App\Http\Controllers\API\Root@index');
 
 $api->group(['middleware' => 'api.auth'], function ($api) {
 	// Endpoints registered here will have the "foo" middleware applied.
