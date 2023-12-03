3e9ccfb23ccede620be51a688ce386c25ec04683
franlara <franlara@192.168.8.123>
2023-12-03 21:12:15 +0100

    Creating the first migration

--- database/migrations/2014_10_12_000000_create_admins_table.php
+++ database/migrations/2014_10_12_000000_create_admins_table.php
@@ -8,15 +8,15 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('password');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('admins');
     }
 };
