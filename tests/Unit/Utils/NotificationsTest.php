1bb469cc1ea9a5e640296118085d910c7f7f6fbe
Fran Lara <fran@digidip.net>
2024-02-27 12:09:14 +0100

    Added new notification to the test

--- tests/Unit/Utils/NotificationsTest.php
+++ tests/Unit/Utils/NotificationsTest.php
@@ -5,46 +5,65 @@ namespace Tests\Unit\Utils\Admins;
 use App\BusinessObjects\DTOs\Users\Admin;
 use App\Notifications\Notification;
 use App\Notifications\User\Admin\Created;
+use App\Notifications\User\Admin\Updated;
 use App\Utils\Notifications as NotificationsUtil;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Notification as FacadeNotification;
-use Tests\TestCase;
+use Illuminate\Support\Str;
 use ReflectionClass;
+use ReflectionException;
+use Tests\TestCase;
 
 class NotificationsTest extends TestCase
 {
-	private const LOCALE = 'test_locale';
+    private const LOCALE = 'test_locale';
 
-	/**
-	 * @dataProvider providerNotification
-	 */
-	public function testSendMailNotification(Notification $notification, string $expectedLocale = '', string $expectedTo = null, string $locale = null, string $to = null): void
-	{
-		if (empty($expectedTo)) {
-			$expectedTo = config('mail.notifications.internal');
-		}
+    /**
+     * @dataProvider providerNotification
+     *
+     * @throws ReflectionException
+     */
+    public function testSendMailNotification(
+        Notification $notification,
+        string $expectedLocale = '',
+        string $expectedTo = null,
+        string $locale = null,
+        string $to = null
+    ): void {
+        if (empty($expectedTo)) {
+            $expectedTo = config('mail.notifications.internal');
+        }
 
-		FacadeNotification::fake();
+        FacadeNotification::fake();
 
-		$notifications = new class() {
-			use NotificationsUtil;
-		};
+        $notifications = new class() {
+            use NotificationsUtil;
+        };
 
-		$class = new ReflectionClass(get_class($notifications));
-		$method = $class->getMethod('sendMailNotification');
-		$method->setAccessible(true);
+        $class = new ReflectionClass(get_class($notifications));
+        $method = $class->getMethod('sendMailNotification');
+        $method->setAccessible(true);
 
-		$method->invokeArgs($notifications, [$notification, $locale, $to]);
+        $method->invokeArgs($notifications, [$notification, $locale, $to]);
 
-		FacadeNotification::assertSentOnDemand(get_class($notification), function (Notification $notification, array $channels, object $notifiable) use ($expectedLocale, $expectedTo) {
-			return ((Str::of($notifiable->routes['mail'])->exactly($expectedTo)) &&
-				(Str::of($notification->locale)->exactly($expectedLocale)));
-		});
-		FacadeNotification::assertCount(1);
-	}
+        FacadeNotification::assertSentOnDemand(get_class($notification),
+            function (Notification $notification, array $channels, object $notifiable) use (
+                $expectedLocale,
+                $expectedTo
+            ) {
+                return ((Str::of($notifiable->routes['mail'])->exactly($expectedTo))
+                        && (Str::of($notification->locale)
+                               ->exactly($expectedLocale)));
+            });
+        FacadeNotification::assertCount(1);
+    }
 
-	public static function providerNotification(): array
-	{
-		return [[new Created(new Admin())], [new Created(new Admin()), self::LOCALE, null, self::LOCALE]]; //[], [true], [true, true]];
-	}
+    public static function providerNotification(): array
+    {
+        return [
+            [new Created(new Admin())],
+            [new Updated(new Admin())],
+            [new Created(new Admin()), self::LOCALE, null, self::LOCALE],
+            [new Updated(new Admin()), self::LOCALE, null, self::LOCALE]
+        ];
+    }
 }
