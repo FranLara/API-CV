5677c74de276d24b30a7a3dbf58c350df4f3895d
franlara <franlara@192.168.8.123>
2024-03-13 00:25:46 +0100

    Added endpoint to request a token and removed unnecessary class

--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,9 @@
 $api->get('', 'App\Http\Controllers\API\Root@index');
 $api->options('allows', 'App\Http\Controllers\API\Root@options');
 
-$api->post('registration', 'App\Http\Controllers\API\Auth\User@register');
-$api->get('token/request', 'App\Http\Controllers\API\Auth\Root@options');
+$api->get('token', 'App\Http\Controllers\API\Auth\Token@request');
 
-$api->group(['prefix' => 'request'], function ($api) {
-	$api->group(['prefix' => 'token'], function ($api) {
-		$api->get('', 'App\Http\Controllers\API\Auth\Token@request');
-	});
-	$api->group(['prefix' => 'account'], function ($api) {
-		$api->post('', 'App\Http\Controllers\API\Auth\User@request');
-	});
-});
+$api->post('account', 'App\Http\Controllers\API\Auth\User@request');
 
 $api->group(['middleware' => 'api.auth'], function ($api) {
 	// Endpoints registered here will have the "foo" middleware applied.
