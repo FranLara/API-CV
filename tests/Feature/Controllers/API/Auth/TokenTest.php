4a1cd25fab559672b039e70059478a338a3db03f
franlara <franlara@192.168.8.123>
2024-03-31 23:56:45 +0200

    Updated test with Laravel json assert

--- tests/Feature/Controllers/API/Auth/TokenTest.php
+++ tests/Feature/Controllers/API/Auth/TokenTest.php
@@ -5,9 +5,10 @@ namespace Tests\Feature\Controllers\API\Auth;
 
 use App\BusinessObjects\DTOs\Utils\Token;
 use App\Http\Controllers\API\API as APIController;
-use Tests\Feature\FeatureTest;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\Feature\Controllers\API\APITest;
 
-class TokenTest extends FeatureTest
+class TokenTest extends APITest
 {
 	private const TYPE_INDEX = 'token_type';
 	private const TOKEN_INDEX = 'access_token';
@@ -18,19 +19,16 @@ class TokenTest extends FeatureTest
 	 */
 	public function testRequest(array $credentials = [], int $expectedSub = 0, string $expectedRole = Token::GUEST_ROLE): void
 	{
-		$payload = json_decode($this->post(env('API_DOMAIN') . '/token', $credentials, [
-			'Accept' => 'application/x.franlara.v1+json'])->getContent(), true);
+		$response = $this->postJson($this->domain . '/token', $credentials, $this->header);
+		$token = json_decode($response->getContent())->{self::TOKEN_INDEX};
+		$payload = app('tymon.jwt')->setToken($token)->getPayload();
 
-		$token = app('tymon.jwt')->setToken($payload[self::TOKEN_INDEX])->getPayload();
-
-		$this->assertIsArray($payload);
-		$this->assertArrayHasKey(self::TYPE_INDEX, $payload);
-		$this->assertArrayHasKey(self::TOKEN_INDEX, $payload);
-		$this->assertEquals($expectedSub, $token->get('sub'));
-		$this->assertSame($expectedRole, $token->get('role'));
-		$this->assertSame('bearer', $payload[self::TYPE_INDEX]);
-		$this->assertArrayHasKey(self::EXPIRATION_INDEX, $payload);
-		$this->assertEquals(3600, $payload[self::EXPIRATION_INDEX]);
+		$this->assertEquals($expectedSub, $payload->get('sub'));
+		$this->assertSame($expectedRole, $payload->get('role'));
+		$response->assertJson(fn (AssertableJson $json) => $json->hasAll([self::TYPE_INDEX, self::TOKEN_INDEX,
+			self::EXPIRATION_INDEX])
+			->where(self::TYPE_INDEX, 'bearer')
+			->where(self::EXPIRATION_INDEX, 3600));
 	}
 
 	public static function providerCredentials(): array
