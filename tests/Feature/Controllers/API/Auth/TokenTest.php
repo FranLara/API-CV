230bb8d15a815331efc067cbe9608c0b53c81850
franlara <franlara@192.168.8.123>
2024-04-10 01:25:23 +0200

    Updated token feature tests

--- tests/Feature/Controllers/API/Auth/TokenTest.php
+++ tests/Feature/Controllers/API/Auth/TokenTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Controllers\API\Auth;
 
 use App\BusinessObjects\DTOs\Utils\Token;
 use App\Http\Controllers\API\API as APIController;
+use Dingo\Api\Http\Response;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\Feature\Controllers\API\APITest;
 
@@ -17,25 +18,30 @@ class TokenTest extends APITest
 	/**
 	 * @dataProvider providerCredentials
 	 */
-	public function testRequest(array $credentials = [], string $expectedRole = Token::GUEST_ROLE): void
+	public function testRequest(array $credentials = [], int $expectedStatusCode = Response::HTTP_OK, string $expectedRole = Token::GUEST_ROLE): void
 	{
 		$response = $this->postJson($this->domain . '/token', $credentials, $this->header);
-		$token = json_decode($response->getContent())->{self::TOKEN_INDEX};
-		$payload = app('tymon.jwt')->setToken($token)->getPayload();
+		$this->assertEquals($expectedStatusCode, $response->getStatusCode());
 
-		$this->assertSame($expectedRole, $payload->get('role'));
-		$response->assertJson(fn (AssertableJson $json) => $json->hasAll([self::TYPE_INDEX, self::TOKEN_INDEX,
-			self::EXPIRATION_INDEX])
-			->where(self::TYPE_INDEX, 'bearer')
-			->where(self::EXPIRATION_INDEX, 3600));
+		if ($response->getStatusCode() == Response::HTTP_OK) {
+			$token = json_decode($response->getContent())->{self::TOKEN_INDEX};
+			$payload = app('tymon.jwt')->setToken($token)->getPayload();
+
+			$this->assertSame($expectedRole, $payload->get('role'));
+			$response->assertJson(fn (AssertableJson $json) => $json->hasAll([self::TYPE_INDEX, self::TOKEN_INDEX,
+				self::EXPIRATION_INDEX])
+				->where(self::TYPE_INDEX, 'bearer')
+				->where(self::EXPIRATION_INDEX, 3600));
+		}
 	}
 
 	public static function providerCredentials(): array
 	{
-		return [[],
+		return [[], [[APIController::PSSWD_PARAMETER => 'test_psswd'], Response::HTTP_UNPROCESSABLE_ENTITY],
+			[[APIController::USERNAME_PARAMETER => 'test_username'], Response::HTTP_UNPROCESSABLE_ENTITY],
 			[[APIController::USERNAME_PARAMETER => 'test_username', APIController::PSSWD_PARAMETER => 'test_psswd']],
 			[
 				[APIController::USERNAME_PARAMETER => env('SUPER_ADMIN_USERNAME'),
-					APIController::PSSWD_PARAMETER => env('SUPER_ADMIN_PASSWORD')], 'admin']];
+					APIController::PSSWD_PARAMETER => env('SUPER_ADMIN_PASSWORD')], Response::HTTP_OK, 'admin']];
 	}
 }
