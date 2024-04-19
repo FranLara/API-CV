d0c90287665a1203b73c999f16a361c2cf5779fe
Fran Lara <fran@digidip.net>
2024-04-19 10:39:11 +0200

    Updated notification to send password

--- app/Notifications/User/Recruiter/Psswd.php
+++ app/Notifications/User/Recruiter/Psswd.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace App\Notifications\User\Recruiter;
 
@@ -7,21 +8,22 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 class Psswd extends Recruiter
 {
-	private const PSSWD_TRANSLATIONS = self::RECRUITER_TRANSLATIONS . 'psswd.';
+    private const string PSSWD_TRANSLATIONS = self::RECRUITER_TRANSLATIONS . 'psswd.';
 
-	public function toMail(object $notifiable): MailMessage
-	{
-		return (new MailMessage())->subject(__(self::PSSWD_TRANSLATIONS . 'subject'))
-			->greeting(__(self::PSSWD_TRANSLATIONS . 'greeting'))
-			->line(__(self::PSSWD_TRANSLATIONS . 'line_1'))
-			->line(__(self::PSSWD_TRANSLATIONS . 'line_2'))
-			->line(__(self::PSSWD_TRANSLATIONS . 'line_3', [
-			'endpoint' => env('API_DOMAIN') . '/token?username=username&password=password']))
-			->line('')
-			->line(__(self::PSSWD_TRANSLATIONS . 'line_4', ['username' => $this->user->getEmail()]))
-			->line(__(self::PSSWD_TRANSLATIONS . 'line_5', ['psswd' => $this->user->getPsswd()]))
-			->line('')
-			->line(__(self::PSSWD_TRANSLATIONS . 'line_6'))
-			->salutation(__(self::PSSWD_TRANSLATIONS . 'salutation', ['email' => env('MAIL_CONTACT')]));
-	}
+    public function toMail(object $notifiable): MailMessage
+    {
+        $endpoint = env('API_DOMAIN') . '/token?username=username&password=password';
+        $acceptHeader = env('API_STANDARDS_TREE') . '.' . env('API_SUBTYPE') . '.' . env('API_VERSION');
+
+        return (new MailMessage())->subject(__(self::PSSWD_TRANSLATIONS . 'subject'))
+                                  ->greeting(__(self::PSSWD_TRANSLATIONS . 'greeting'))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_1'))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_2', ['endpoint' => $endpoint]))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_3',
+                                      ['username' => $this->user->getEmail()]))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_4', ['psswd' => $this->user->getPsswd()]))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_5'))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_6', ['accept' => $acceptHeader]))
+                                  ->line(__(self::PSSWD_TRANSLATIONS . 'line_7', ['email' => env('MAIL_CONTACT')]));
+    }
 }
--- lang/es/notification.php
+++ lang/es/notification.php
@@ -1,18 +1,39 @@
 <?php
-return ['regards' => 'Un saludo', 'rights' => 'Todos los derechos reservados.',
-	'user' => ['greeting' => '¡Aviso!', 'line_2' => 'Su idioma escogido es el: ":language"',
-		'admin' => [
-			'creation' => ['subject' => '¡Un nuevo administrador :username creado!',
-				'line_1' => 'La plataforma ha registrado un nuevo administrador: ":username"',],
-			'update' => ['subject' => '¡Administrador :username actualizado!',
-				'line_1' => 'El administrador ":username" fue actualizado.',]],
-		'recruiter' => [
-			'creation' => ['subject' => '¡Un nuevo reclutador :email creado!',
-				'line_1' => 'La API ha registrado un nuevo reclutador: ":email"',],
-			'psswd' => ['subject' => 'User for Fran Lara CV API created!', 'greeting' => 'Welcome',
-				'line_1' => 'Your user to call the Fran Lara CV API has been created.',
-				'line_2' => 'To make the calls to authenticated endpoints, you will need to request and use a JWT.',
-				'line_3' => 'To request a JWT you need to call this endpoint :endpoint with the following credentials:',
-				'line_4' => 'Username: :username', 'line_5' => 'Passsword: :psswd',
-				'line_6' => 'Do not forget to add to request header "Authorization: Bearer " and the JWT.',
-				'salutation' => 'If you have any doubt, do not hesitate to ask me writing to :email']]]];
+
+return [
+    'regards' => 'Un saludo',
+    'rights'  => 'Todos los derechos reservados.',
+    'user'    => [
+        'greeting'  => '¡Aviso!',
+        'line_2'    => 'Su idioma escogido es el: ":language"',
+        'admin'     => [
+            'creation' => [
+                'subject' => '¡Un nuevo administrador :username creado!',
+                'line_1'  => 'La plataforma ha registrado un nuevo administrador: ":username"',
+            ],
+            'update'   => [
+                'subject' => '¡Administrador :username actualizado!',
+                'line_1'  => 'El administrador ":username" fue actualizado.',
+            ]
+        ],
+        'recruiter' => [
+            'creation' => [
+                'subject' => '¡Un nuevo reclutador :email creado!',
+                'line_1'  => 'La API ha registrado un nuevo reclutador: ":email"',
+            ],
+            'psswd'    => [
+                'subject'  => '¡Usuario para la API del CV de Fran Lara creado!',
+                'greeting' => 'Bienvenido/a',
+                'line_1'   => 'Su usuario para llamar a la API del CV de Fran Lara ha sido creado. '
+                              . 'Para llamar a los endpoints que requieren autenticación, necesitará obtener y usar un JWT. '
+                              . 'Para solicitar un JWT, necesita llamar al siguiente endpoint con el usuario y contraseña dados:',
+                'line_2'   => 'Endpoint: :endpoint',
+                'line_3'   => 'Username: :username',
+                'line_4'   => 'Passsword: :psswd',
+                'line_5'   => 'No olvide añadir al header de la petición:',
+                'line_6'   => '"Accept: application/:accept+json" y "Authorization: Bearer " y el JWT.',
+                'line_7'   => 'Si tiene alguna pregunta, no dude en escribirme a :email'
+            ]
+        ]
+    ]
+];
