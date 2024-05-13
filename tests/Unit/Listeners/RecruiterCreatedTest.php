2db1d0d79b17255abaeac3d53f82a13d7b16604b
Fran Lara <fran@digidip.net>
2024-05-13 13:29:44 +0200

    Added declared strict

--- tests/Unit/Listeners/RecruiterCreatedTest.php
+++ tests/Unit/Listeners/RecruiterCreatedTest.php
@@ -2,16 +2,21 @@
 
 declare(strict_types=1);
 
-namespace App\Listeners;
+namespace Tests\Unit\Listeners;
 
 use App\Events\RecruiterCreated as RecruiterCreatedEvent;
 use App\Notifications\User\Recruiter\Created;
 use App\Notifications\User\Recruiter\Psswd;
 use App\Utils\Notifications as NotificationUtils;
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Tests\TestCase;
 
-class RecruiterCreated extends Listener
+class RecruiterCreatedTest extends TestCase
 {
     use NotificationUtils;
+
+    public string $queue = 'listeners';
 
     public function handle(RecruiterCreatedEvent $event): void
     {
