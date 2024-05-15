a9a9867f606b842cec120b94a441616fa09b84ba
franlara <franlara@192.168.8.123>
2024-05-15 02:02:28 +0200

    Added saver and tests

--- app/BusinessObjects/Models/Users/Recruiter.php
+++ app/BusinessObjects/Models/Users/Recruiter.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\BusinessObjects\Models\Users;
 
-use App\Events\ModelSaved;
 use Database\Factories\RecruiterFactory;
 
 class Recruiter extends User
 {
-    protected $dispatchesEvents = ['saved' => ModelSaved::class];
-
     protected $fillable = ['id', 'email', 'name', 'password', 'language', 'linkedin_profile'];
 
     protected static function newFactory(): RecruiterFactory
--- tests/Feature/Controllers/API/Auth/UserTest.php
+++ tests/Feature/Controllers/API/Auth/UserTest.php
@@ -1,44 +1,42 @@
 <?php
-
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Tests\Feature\Controllers\API\Auth;
 
 use App\Http\Controllers\API\API as APIController;
 use Dingo\Api\Http\Response;
 use Tests\Feature\Controllers\API\APITest;
+use function collect;
 
 class UserTest extends APITest
 {
-    /**
-     * @dataProvider providerUsers
-     */
-    public function testRequest(int $expectedStatusCode = Response::HTTP_UNPROCESSABLE_ENTITY, string $email = ''): void
-    {
-        $response = $this->post($this->domain . '/account', $this->getUser($email), $this->getHeader());
 
-        $this->assertEquals($expectedStatusCode, $response->getStatusCode());
+	/**
+	 * @dataProvider providerUsers
+	 */
+	public function testRequest(int $expectedStatusCode = Response::HTTP_UNPROCESSABLE_ENTITY, string $email = ''): void
+	{
+		$response = $this->post($this->domain . '/account', $this->getUser($email), $this->getHeader());
 
-        if ($response->getStatusCode() == Response::HTTP_CREATED) {
-            $this->assertDatabaseCount('jobs', 1);
-            $this->assertDatabaseCount('recruiters', 1);
-            $this->assertDatabaseHas('jobs', ['queue' => 'listeners']);
-            $this->assertDatabaseHas('recruiters', ['email' => $email]);
-        }
-    }
+		$this->assertEquals($expectedStatusCode, $response->getStatusCode());
 
-    public static function providerUsers(): array
-    {
-        return [[], [Response::HTTP_CREATED, 'test@recruiter.com']];
-    }
+		if ($response->getStatusCode() == Response::HTTP_CREATED) {
+			$this->assertDatabaseCount('jobs', 2);
+			$this->assertDatabaseCount('recruiters', 1);
+			$this->assertDatabaseHas('jobs', ['queue' => 'listeners']);
+			$this->assertDatabaseHas('recruiters', ['email' => $email]);
+		}
+	}
 
-    private function getUser(string $email): array
-    {
-        return [
-            APIController::EMAIL_PARAMETER    => $email,
-            APIController::LINKEDIN_PARAMETER => fake()->url,
-            APIController::NAME_PARAMETER     => fake()->name,
-            APIController::LANGUAGE_PARAMETER => collect(['es', 'en'])->random(),
-        ];
-    }
+	public static function providerUsers(): array
+	{
+		return [[], [Response::HTTP_CREATED, 'test@recruiter.com']];
+	}
+
+	private function getUser(string $email): array
+	{
+		return [APIController::EMAIL_PARAMETER => $email, APIController::LINKEDIN_PARAMETER => fake()->url,
+			APIController::NAME_PARAMETER => fake()->name,
+			APIController::LANGUAGE_PARAMETER => collect(['es', 'en'])->random(),];
+	}
 }
