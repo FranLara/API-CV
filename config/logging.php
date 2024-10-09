01dad927e006bf20ce65cf7941ea250041fa3b0d
Fran Lara <francisco.lara.casalilla@gmail.com>
2024-10-09 23:15:28 +0200

    Updated vite dependency

--- config/logging.php
+++ config/logging.php
@@ -1,4 +1,5 @@
 <?php
+
 return [ /*
            |--------------------------------------------------------------------------
            | Default Log Channel
@@ -10,36 +11,41 @@ return [ /*
            |
            */
 
-'default' => env('LOG_CHANNEL', 'credentials'),
+         'default' => env('LOG_CHANNEL', 'credentials'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Deprecations Log Channel
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the log channel that should be used to log warnings
-    | regarding deprecated PHP and library features. This allows you to get
-    | your application ready for upcoming major versions of dependencies.
-    |
-    */
+         /*
+         |--------------------------------------------------------------------------
+         | Deprecations Log Channel
+         |--------------------------------------------------------------------------
+         |
+         | This option controls the log channel that should be used to log warnings
+         | regarding deprecated PHP and library features. This allows you to get
+         | your application ready for upcoming major versions of dependencies.
+         |
+         */
 
-    'deprecations' => ['channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'), 'trace' => false,],
+         'deprecations' => ['channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'), 'trace' => false,],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Log Channels
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log channels for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Drivers: "single", "daily", "slack", "syslog",
-    |                    "errorlog", "monolog",
-    |                    "custom", "stack"
-    |
-    */
+         /*
+         |--------------------------------------------------------------------------
+         | Log Channels
+         |--------------------------------------------------------------------------
+         |
+         | Here you may configure the log channels for your application. Out of
+         | the box, Laravel uses the Monolog PHP logging library. This gives
+         | you a variety of powerful log handlers / formatters to utilize.
+         |
+         | Available Drivers: "single", "daily", "slack", "syslog",
+         |                    "errorlog", "monolog",
+         |                    "custom", "stack"
+         |
+         */
 
-	'channels' => [
-		'credentials' => ['driver' => 'single', 'path' => storage_path('logs/wrong-credentials.log'),
-			'replace_placeholders' => true]]];
+         'channels' => [
+             'credentials' => [
+                 'driver'               => 'single',
+                 'path'                 => storage_path('logs/wrong-credentials.log'),
+                 'replace_placeholders' => true
+             ]
+         ]
+];
