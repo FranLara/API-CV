18d1cc7e09839332789382626f267f6d814432bc
Fran Lara <francisco.lara.casalilla@gmail.com>
2025-11-24 00:41:02 +0100

    Added feature test for existing users

--- tests/Feature/Controllers/API/Auth/UserTest.php
+++ tests/Feature/Controllers/API/Auth/UserTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Controllers\API\Auth;
 
+use App\BusinessObjects\Models\Users\Recruiter;
+use App\BusinessObjects\Models\Users\Technician;
 use App\Http\Controllers\API\API as APIController;
 use Dingo\Api\Http\Response;
+use Illuminate\Support\Facades\DB;
 use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\Feature\Controllers\API\APITests;
 
@@ -13,16 +16,21 @@ use function collect;
 
 class UserTest extends APITests
 {
+    private const string EXISTING_RECRUITER = 'test@existingrecruiter.com';
+    private const string EXISTING_TECHNICIAN = 'test@existingtechnician.com';
+
     #[DataProvider('providerUsers')]
     public function testRequest(int $expectedStatusCode, string $email = ''): void
     {
+        $this->initializeTest();
+
         $response = $this->post($this->domain . '/accounts', $this->getUser($email), $this->getHeader());
 
         $this->assertEquals($expectedStatusCode, $response->getStatusCode());
 
         if ($response->getStatusCode() == Response::HTTP_CREATED) {
             $this->assertDatabaseCount('jobs', 2);
-            $this->assertDatabaseCount('recruiters', 1);
+            $this->assertDatabaseCount('recruiters', 2);
             $this->assertDatabaseHas('jobs', ['queue' => 'listeners']);
             $this->assertDatabaseHas('recruiters', ['email' => $email]);
         }
@@ -30,7 +38,19 @@ class UserTest extends APITests
 
     public static function providerUsers(): array
     {
-        return [[Response::HTTP_UNPROCESSABLE_ENTITY], [Response::HTTP_CREATED, 'test@recruiter.com']];
+        return [
+            [Response::HTTP_UNPROCESSABLE_ENTITY],
+            [Response::HTTP_CREATED, 'test@recruiter.com'],
+            [Response::HTTP_UNPROCESSABLE_ENTITY, self::EXISTING_RECRUITER],
+            [Response::HTTP_UNPROCESSABLE_ENTITY, self::EXISTING_TECHNICIAN],
+        ];
+    }
+
+    private function initializeTest(): void
+    {
+        Recruiter::factory()->create(['email' => self::EXISTING_RECRUITER]);
+        Technician::factory()->create(['email' => self::EXISTING_TECHNICIAN]);
+        DB::table('jobs')->truncate();
     }
 
     private function getUser(string $email): array
