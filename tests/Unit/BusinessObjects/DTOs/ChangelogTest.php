d3e91a1acd1c440743239e8324dd5b055871ceef
Fran Lara <francisco.lara.casalilla@gmail.com>
2026-04-28 10:25:43 +0200

    Added missing unit tests

---
+++ tests/Unit/BusinessObjects/DTOs/ChangelogTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\BusinessObjects\DTOs;
+
+use App\BusinessObjects\DTOs\Changelog;
+use PHPUnit\Framework\TestCase;
+
+class ChangelogTest extends TestCase
+{
+    private const string TYPE = 'test_type';
+    private const string ENTITY_ID = 'test_entity_id';
+    private const string VALUE_PAYLOAD = 'test_value_payload';
+
+    public function testConstructor(): void
+    {
+        $changelog = $this->getChangelog();
+
+        $this->assertNull($changelog->getIdentifier());
+        $this->assertSame(self::TYPE, $changelog->getType());
+        $this->assertSame(self::ENTITY_ID, $changelog->getEntityId());
+        $this->assertSame(self::VALUE_PAYLOAD, $changelog->getValuePayload());
+    }
+
+    public function testGetType(): void
+    {
+        $this->assertSame(self::TYPE, $this->getChangelog()->getType());
+    }
+
+    public function testGetEntityId(): void
+    {
+        $this->assertSame(self::ENTITY_ID, $this->getChangelog()->getEntityId());
+    }
+
+    public function testGetValuePayload(): void
+    {
+        $this->assertSame(self::VALUE_PAYLOAD, $this->getChangelog()->getValuePayload());
+    }
+
+    private function getChangelog(): Changelog
+    {
+        return new Changelog(type: self::TYPE, entityId: self::ENTITY_ID, valuePayload: self::VALUE_PAYLOAD);
+    }
+}
