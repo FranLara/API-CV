7cf39f19ceda6c9078243a8d118c98f30a7d5030
Fran Lara <francisco.lara.casalilla@gmail.com>
2026-05-12 22:05:03 +0200

    Updated migration files with Pint conventions

--- database/migrations/2024_04_01_125417_create_recruiters_table.php
+++ database/migrations/2024_04_01_125417_create_recruiters_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('recruiters', function (Blueprint $table) {
@@ -14,7 +15,7 @@ return new class extends Migration {
             $table->string('password');
             $table->string('language');
             $table->string('linkedin_profile')->nullable();
-            $table->timestamp('created_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
--- database/migrations/2025_08_24_220953_create_technicians_table.php
+++ database/migrations/2025_08_24_220953_create_technicians_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('technicians', function (Blueprint $table) {
@@ -15,7 +16,7 @@ return new class extends Migration {
             $table->string('language');
             $table->string('github_profile')->nullable();
             $table->string('linkedin_profile')->nullable();
-            $table->timestamp('created_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
